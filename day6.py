8817f552376541bdc964137247f117eb7c109b85
Irfan031306 <erfanhsbc@gmail.com>
2024-09-02 08:04:10 +0100

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,18 @@
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+username = input("Username > ")
+password = input("Password > ")
+if username == "David" and password == "totallyNotBald":
+  print("Why hello there David, what a lovely accent you have, you could   have charmed your way in here even without a password.")
+  print("Have a great day.")
+  print("Don't forget to wear a hat in the sun!")
+elif username == "Irfan" and password == "Irfan321":
+ print( "Hello Irfan, I'm sure your the new IT admin here and recently started to learn coading. I hope you have a great day!")
+ print("Have a good training session")
+ print("Get going Irfan")
+elif username == "Hussain" and password == "thecoolestman":
+ print("Hello Hussain, We've heard about you alot in the last meeting  and here we do it time to meet you today, We're glad you made it today and welcome to your office!")
+ print("We are have a meeting today and we are looking forward to meeting you in person")
+ print("Have a great day")
+else:
+  print("Please contact your administrator")
